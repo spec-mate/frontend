c8081de6a48c9b59fcb9c385dcfbec67619d9448
se1park <swon7150@gmail.com>
2025-09-18 14:20:56 +0900

    0918 수정사항 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
 import "./styles/Home.css";
 import { useHeaderStore } from "../store/headerStore";
+import { useNavigate } from "react-router-dom"; // ✅ 추가
 
 export default function Home() {
   const setHeaderVersion = useHeaderStore((state) => state.setHeaderVersion);
+  const navigate = useNavigate(); // ✅ 라우터 네비게이터 생성
 
   useEffect(() => {
     setHeaderVersion("white");
   }, [setHeaderVersion]);
+
+  const handleClick = () => {
+    navigate("/usage"); // ✅ 클릭 시 /usage 페이지로 이동
+  };
 
   return (
     <div className="home">
@@ -26,7 +32,9 @@ export default function Home() {
             복잡한 PC 부품과 설명 때문에 난감하셨나요? <br />
             이제는 스펙메이트로 한번에 해결하세요!
           </h6>
-          <button className="main-button">나만의 PC 조립하기</button>
+          <button className="main-button" onClick={handleClick}>
+            나만의 PC 조립하기
+          </button>
         </div>
       </main>
     </div>
